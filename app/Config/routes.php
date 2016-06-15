16e82ed7f5611163f551eee833dd2b64ca88b3a5
oudomseila <oudomseilaphok@gmail.com>
2016-06-15 09:38:46 +0700

    completed

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,5 +1,16 @@
 <?php
 Router::mapResources(array('sportclubs'));
+//Router::resourceMap(array(
+//    array('action' => 'index', 'method' => 'GET', 'id' => false),
+//    array('action' => 'view', 'method' => 'GET', 'id' => true),
+//    array('action' => 'add', 'method' => 'POST', 'id' => false),
+//    array('action' => 'edit', 'method' => 'PUT', 'id' => true),
+//    array('action' => 'update', 'method' => 'POST', 'id' => true),
+//    array('action' => 'delete', 'method' => 'DELETE', 'id' => true),
+//
+//    // sportclubs controller
+//    array('controller'=>'sportclubs', 'action' => 'login', 'method' => 'POST', 'id' => false)
+//));
 /*
 Router::resourceMap(array(
     array('action' => 'index', 'method' => 'GET', 'id' => false),
